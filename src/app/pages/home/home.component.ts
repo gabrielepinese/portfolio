7951a3e3bc34fe0195a11e4e4fd4bb3698841169
gabrielepinese <84739941+gabrielepinese@users.noreply.github.com>
2025-05-14 17:00:26 +0200

    base layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,8 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  blobPath1 = 'M0,0 C500,200 1000,0 1500,200 L1500,1000 L0,1000 Z';
-  blobPath2 = 'M0,50 C500,250 1000,50 1500,250 L1500,1000 L0,1000 Z';
-  blobPath3 = 'M0,100 C500,300 1000,100 1500,300 L1500,1000 L0,1000 Z';
-}
+export class HomeComponent {}
