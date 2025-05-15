b7d5cda1153f3faf7b88c5d93f8f915437fe948f
gabrielepinese <84739941+gabrielepinese@users.noreply.github.com>
2025-05-15 16:13:59 +0200

    general update

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -8,4 +14,28 @@ import { CommonModule } from '@angular/common';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  @ViewChild('observeTarget') observeTarget!: ElementRef;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              this.observeTarget.nativeElement.classList.add('visible');
+              observer.disconnect(); // opzionale: se vuoi osservarlo solo una volta
+            }
+          });
+        },
+        { threshold: 0.2 }
+      );
+
+      if (this.observeTarget?.nativeElement) {
+        observer.observe(this.observeTarget.nativeElement);
+      }
+    }
+  }
+}
