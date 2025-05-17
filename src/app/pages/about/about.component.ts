8a22568bb38238fc536077b2faa8b5d6e0b3af51
gabrielepinese <84739941+gabrielepinese@users.noreply.github.com>
2025-05-17 21:03:50 +0200

    general update

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-about',
-    imports: [CommonModule],
-    templateUrl: './about.component.html',
-    styleUrl: './about.component.scss'
+  selector: 'app-about',
+  imports: [CommonModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'assets/pdf/CV -Gabriele-Pinese.pdf';
+    link.download = 'CV -Gabriele-Pinese.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+}
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-contact',
-    imports: [CommonModule],
-    templateUrl: './contact.component.html',
-    styleUrl: './contact.component.scss'
+  selector: 'app-contact',
+  imports: [CommonModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  sendMail() {
+    window.location.href =
+      'mailto:gabrielepinese97@gmail.com?subject=Portfolio%20Request&body=';
+  }
+}
