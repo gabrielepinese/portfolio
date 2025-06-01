e9561c09baca0ae708e24e2b8077643eeee32e6f
gabrielepinese <84739941+gabrielepinese@users.noreply.github.com>
2025-06-01 12:26:32 +0200

    add work experience

--- src/app/pages/works/works.component.ts
+++ src/app/pages/works/works.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemListComponent } from '../../shared/ui/item-list/item-list.component';
 
 @Component({
-    selector: 'app-works',
-    imports: [CommonModule],
-    templateUrl: './works.component.html',
-    styleUrl: './works.component.scss'
+  selector: 'app-works',
+  imports: [CommonModule, ItemListComponent],
+  templateUrl: './works.component.html',
+  styleUrl: './works.component.scss',
 })
 export class WorksComponent {}
---
+++ src/app/shared/ui/item-list/item-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-item-list',
+  imports: [],
+  templateUrl: './item-list.component.html',
+  styleUrl: './item-list.component.scss',
+})
+export class ItemListComponent {
+  link = input.required<string>();
+  from = input.required<string>();
+  to = input.required<string>();
+  company = input.required<string>();
+  title = input.required<string>();
+  desc = input.required<string>();
+  chips = input.required<string[]>();
+
+  goto(url: string) {
+    window.open(url);
+  }
+}
