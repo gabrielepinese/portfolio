afd6d586428ebe1a811ffbd6a7dd299d0a93ea96
gabrielepinese <84739941+gabrielepinese@users.noreply.github.com>
2026-05-01 14:25:45 +0200

    chore: update Angular 20.3 -> 21.2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
+
 
 @Component({
   selector: "app-home",
-  imports: [CommonModule],
+  imports: [],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
--- src/app/pages/works/works.component.ts
+++ src/app/pages/works/works.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
+
 import { ItemListComponent } from "../../shared/ui/item-list/item-list.component";
 import { LIST_ITEMS, CompanyList } from "../../core/models/company-list";
 
 @Component({
   selector: "app-works",
-  imports: [CommonModule, ItemListComponent],
+  imports: [ItemListComponent],
   templateUrl: "./works.component.html",
   styleUrl: "./works.component.scss",
 })
